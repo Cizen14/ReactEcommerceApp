f8b541b621e94f5d628f6c55c9d8e85124a65661
Cizen14 <cizenmah@gmail.com>
2024-01-10 14:36:49 +1100

    added the footer and adjusted some of the css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ShopCategory from './Pages/ShopCategory';
 import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
           <Route path='/login' element={<LoginSignup/>}/>
         </Routes>
       </BrowserRouter>
+      <Footer/>
       
     </div>
   );
