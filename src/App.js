be67263b17d04ce1e2b5b3e9439c868c5d5aba53
Cizen14 <cizenmah@gmail.com>
2024-01-13 13:18:12 +1100

    solved the issue with the banner

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
-import men_banner from './Components/Assets/banner_mens.png';
-import women_banner from './Components/Assets/banner_women.png';
-import kids_banner from './Components/Assets/banner_kids.png';
-
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -20,8 +19,8 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCategory banner={men_banner} category= "mens"/>}/>
-          <Route path='/womens' element={<ShopCategory banner={women_banner} category= "womens"/>}/>
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category= "men"/>}/>
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category= "women"/>}/>
           <Route path='/kids' element={<ShopCategory banner={kids_banner} category= "kid"/>}/>
           <Route path='/product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
