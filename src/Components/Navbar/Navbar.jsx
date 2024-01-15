6ec52a68e5c3f97e247402133a0f856832243019
Cizen14 <cizenmah@gmail.com>
2024-01-16 10:43:17 +1100

    added the login page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
             <p>Shopaholic</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu ==="shop"?<hr/>:<></>} </li>
-            <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}}to='/mens'>Men</Link>{menu ==="mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:'none'}}to='/womens'>Women</Link>{menu ==="womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:'none'}}to='/kids'>Kids</Link>{menu ==="kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><Link  class="text-secondary" style={{textDecoration:'none'}} to='/'>Shop</Link>{menu ==="shop"?<hr/>:<></>} </li>
+            <li onClick={()=>{setMenu("mens")}}><Link class="text-secondary" style={{textDecoration:'none'}}to='/mens'>Men</Link>{menu ==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><Link class="text-secondary" style={{textDecoration:'none'}}to='/womens'>Women</Link>{menu ==="womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link class="text-secondary" style={{textDecoration:'none'}}to='/kids'>Kids</Link>{menu ==="kids"?<hr/>:<></>}</li>
         </ul>
         <ul className='nav-login-cart'>
             <Link to='/login'><button>Login</button> </Link>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <section>
+    <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" >
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                alt="login form" class="img-fluid" />
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+                <form action='' >
+
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <i class="fas fa-cubes fa-2x me-3"></i>
+                    <span class="h1 fw-bold mb-0">Logo</span>
+                  </div>
+
+                  <h5 class="fw-normal mb-3 pb-3" >Sign into your account</h5>
+
+                  <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example17">Email address</label>
+                    <input type="email" name='email' id="form2Example17" class="form-control form-control-lg" />
+                   
+                    
+                   
+                  </div>
+
+                  <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example27">Password</label>
+                    <input type="password" name='password' id="form2Example27"  class="form-control form-control-lg" />
+ 
+                    
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                  </div>
+
+                  <a class="small text-muted" href="#!">Forgot password?</a>
+                  <p class="mb-5 pb-lg-2">Don't have an account? <Link to= '/signup' className='reg'
+                      >Register here</Link></p>
+                  <a href="#!" class="small text-muted">Terms of use.</a>
+                  <a href="#!" class="small text-muted">Privacy policy</a>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
   )
 }
 
