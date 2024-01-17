85435731658d98d6a45dca328fff151837b3fd36
Cizen14 <cizenmah@gmail.com>
2024-01-17 14:18:43 +1100

    completed with the cart thing

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from "react";
 import all_product from '../Components/Assets/all_product'
 
+
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () =>{
@@ -24,8 +25,37 @@ const ShopContextProvider = (props)=>{
         
     }
 
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                let itemInfo= all_product.find((product) => product.id === Number(item))
+                totalAmount += cartItems[item] * itemInfo.new_price;
+            }
+            
+        }
+        return totalAmount;
+    }
 
-    const contextValue = {all_product, cartItems, addToCart, removeFromCart};
+        const getTotalCartItems = () =>{
+            let totalItem = 0;
+            for(const item in cartItems){
+                if(cartItems[item]>0)
+                {
+                    totalItem+= cartItems[item];
+                }
+            }
+            return totalItem;
+        
+        }
+        
+
+    
+
+
+    const contextValue = { getTotalCartItems, getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
     return(
         <ShopContext.Provider value={contextValue}>
             {props.children}
