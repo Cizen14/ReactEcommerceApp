7c58b9266d6fd4ecb836c2bf8efd0937bbcc807c
Cizen14 <cizenmah@gmail.com>
2024-01-21 12:24:31 +1100

    responsive screen done for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,17 +4,28 @@ import logo from '../Assets/herologo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/down-arrow.png'
+
 const Navbar = () => {
   const [menu, setMenu]=useState("shop")
   const {getTotalCartItems} = useContext(ShopContext);
+  
+
 
   const menuRef = useRef();
+
+  const dropdown_toggle = (e) =>{
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+
+  }
   return (
     <div className='navbar'>
         <div className='nav-logo'>
             <img src={logo} alt=''/>
             <p>Shopaholic</p>
         </div>
+        <img className='nav-dropdown' src={nav_dropdown} onClick={dropdown_toggle} alt='' />
         <ul ref={menuRef} className='nav-menu'>
             <li onClick={()=>{setMenu("shop")}}><Link  class="text-secondary" style={{textDecoration:'none'}} to='/'>Shop</Link>{menu ==="shop"?<hr/>:<></>} </li>
             <li onClick={()=>{setMenu("mens")}}><Link class="text-secondary" style={{textDecoration:'none'}}to='/mens'>Men</Link>{menu ==="mens"?<hr/>:<></>}</li>
