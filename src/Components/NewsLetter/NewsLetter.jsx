81c97e6437bf2fcb70e56740509d1a47247750ba
Cizen14 <cizenmah@gmail.com>
2024-01-24 15:07:47 +1100

    done with the newletter

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './NewsLetter.css'
-import { Button, Stack, TextField } from '@mui/material'
+
 const NewsLetter = () => {
   return (
    
     <div className='newsletter'>
         <h1>Get Exclusive Offers On Your Email</h1>
         <p> Subscribe to our newsltter and stay updated</p>
-        <Stack>
-            <TextField id="email" label="Your Email Id"  />
-            <Button  color="inherit" variant ="text" className='btn'>Subscribe</Button>
-         </Stack>
+       <div>
+          <input type='email' placeholder='Your Email' />
+          <button> Subscribe</button>
+        </div>
     </div>
   
 
